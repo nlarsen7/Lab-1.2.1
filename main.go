78bf876086ac93ee108ee5b8fe9a6cc444cb5ee8
Nicholas Larsen <nlarsen2013@gmail.com>
2020-01-31 03:19:56 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+// Programmer name: Nicholas Larsen
+// Date completed:  1/30/2020
+// Description: Lab 1.2.1
+
+package main
+
+import "fmt" 
+
+func main() {
+    fmt.Println("Hello world!")
+      fmt.Println("My name is Nicholas Larsen, and my major is Computer Science.")
+    fmt.Println("This is my third semester at Butte College.")
+  fmt.Println("One of my favorite pastimes is playing soccer.")
+}
